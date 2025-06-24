53705e1ded3c783e9c2cdcb4ab43fe598fe92254
tl-brcm <149362588+tl-brcm@users.noreply.github.com>
2025-06-24 11:12:35 +0800

    Remove debug prints and logger calls from config

--- config.py
+++ config.py
@@ -11,12 +11,3 @@ SITE_MINDER_USERNAME = os.getenv("SITE_MINDER_USERNAME")
 SITE_MINDER_PASSWORD = os.getenv("SITE_MINDER_PASSWORD")
 VERIFY_SSL = os.getenv("VERIFY_SSL", "false").lower() == "true"
 
-print("[DEBUG] Loaded config:")
-print("SITE_MINDER_BASE_URL =", SITE_MINDER_BASE_URL)
-print("SITE_MINDER_USERNAME =", SITE_MINDER_USERNAME)
-print("VERIFY_SSL =", VERIFY_SSL)
-
-logger.debug(f"BASE_URL = {SITE_MINDER_BASE_URL}")
-logger.debug(f"USERNAME = {SITE_MINDER_USERNAME}")
-logger.debug(f"PASSWORD = {bool(SITE_MINDER_PASSWORD)}")
-logger.debug(f"VERIFY_SSL = {VERIFY_SSL}") 
